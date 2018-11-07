ee3f90fb36f59ed346a0dab6a133625db3b358ec
Deniz Beser <Deniz>
2018-11-07 10:56:50 -0500

    open vids

--- run.py
+++ run.py
@@ -6,5 +6,25 @@ easy = './Easy.mp4'
 medium = './Medium.mp4'
 hard = './hard.mp4'
 
+video = cv2.VideoCapture(easy)
+
+success, prevframe = video.read()
+prev_grayframe = cv2.cvtColor(prevframe, cv2.COLOR_BGR2GRAY)
+success, frame = video.read()
+
+# cv2.imshow('result',frame)
+# if cv2.waitKey(0) & 0xff == 27:
+# 	cv2.destroyAllWindows()
+
+while success:
+	grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)	
+	
+	# Do whatever
+	
+	# get next frame
+	prev_grayframe = grayframe.copy()
+	success, frame = video.read()
+
+video.release()
 
 
