3a98f1a10deb75d87113f5dc4259cbb5574cbfe7
mchiquier <chiquier@seas.upenn.edu>
2018-11-07 08:24:29 -0800

    skeleton for getFeatuers.py

--- getFeatures.py
+++ getFeatures.py
@@ -1,1 +1,8 @@
 '''getFeatures.py file  '''
+import cv2
+from skimage import feature.corner_shi_tomasi
+
+def getFeatures(img, bbox) : 
+    image_response = feature.corner_shi_tomasi(img)
+    corners_in_bbox = image_response[bbox]
+    return corners_in_bbox
